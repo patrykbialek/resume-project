16dec23955a8f1d382fac7e1abbe5b6c93fb13c4
Patryk Bialek <patryk.b@me.com>
2015-09-15 21:42:53 +0200

    Update mock-data.js

--- src/client/test-helpers/mock-data.js
+++ src/client/test-helpers/mock-data.js
@@ -14,142 +14,142 @@ var mockData = (function () {
     function getData() {
         return {
             "bio" : {
-      "born" : {
-        "icon" : "room",
-        "label" : "Born",
-        "name" : "Jan 1990",
-        "order" : 2
-      },
-      "email" : {
-        "email" : true,
-        "icon" : "email",
-        "label" : "Email",
-        "name" : "resume-project@patrykbe.pl",
-        "order" : 5
-      },
-      "family" : {
-        "icon" : "people",
-        "label" : "Family",
-        "name" : "wife, five kids",
-        "order" : 3
-      },
-      "name" : {
-        "icon" : "person",
-        "label" : "Name",
-        "name" : "John Doe",
-        "order" : 1
-      },
-      "phone" : {
-        "icon" : "call",
-        "label" : "Phone",
-        "name" : "+48 510217852",
-        "order" : 4
-      }
-    },
-    "education" : {
-      "u1" : {
-        "field" : "economy, speciality banking — MA",
-        "name" : "univeristy",
-        "order" : 1,
-        "when" : "2001 - 2002"
-      },
-      "u2" : {
-        "field" : "finance/banking, speciality banking — LIC",
-        "name" : "univeristy",
-        "order" : 2,
-        "when" : "1999 - 2001"
-      }
-    },
-    "experience" : {
-      "exp1" : {
-        "company" : "Company 1",
-        "order" : 2,
-        "position" : "IT Specialist",
-        "responsibilities" : {
-          "res1" : "responsibility 1  ",
-          "res2" : "responsibility 2 "
-        },
-        "when" : "Oct 1998 - Jan 1999 (4 mths)"
-      },
-      "exp2" : {
-        "company" : "Company 2",
-        "order" : 1,
-        "position" : "Analyst",
-        "responsibilities" : {
-          "res1" : "responsibility 1  ",
-          "res2" : "responsibility 2 "
-        },
-        "when" : "Nov 1999 - present (15 yrs)"
-      }
-    },
-    "faq" : {
-      "faq01" : {
-        "answer" : "anwser 1",
-        "label" : "Answer 1",
-        "order" : 1,
-        "question" : "Question 1?"
-      },
-      "faq02" : {
-        "answer" : "anwser 2",
-        "label" : "Answer 2",
-        "order" : 2,
-        "question" : "Question 2?"
-      },
-      "faq03" : {
-        "answer" : "anwser 3",
-        "label" : "Answer 3",
-        "order" : 3,
-        "question" : "Question 3?"
-      }
-    },
-    "interests" : "computer technology; electronic gadget news; winter sports; bikes; motor sport (wec, dtm, f1)",
-    "languages" : {
-      "de" : "german ",
-      "en" : "english"
-    },
-    "profile" : {
-      "desc" : "I am interested in work in the Information Systems area. I am interested in work in the Information Systems area. I am interested in work in the Information Systems area. <br><br>I am interested in work in the Information Systems area. I am interested in work in the Information Systems area. I am interested in work in the Information Systems area. "
-    },
-    "projects" : {
-      "project one" : {
-        "client" : "Company - IT dept.",
-        "desc" : "Tool supporting test process. Allows user to create tasks list per system version, application or business unit.",
-        "image" : "",
-        "name" : "Project one",
-        "order" : 1,
-        "other" : "internal application",
-        "period" : "2010/2011",
-        "status" : "finished",
-        "subname" : "test task manager",
-        "tasks" : "analysis, layout, frontend code, tests",
-        "team" : "two people",
-        "technology" : ".Net, MS SQL, HTML, CSS, JavaScript",
-        "type" : "application"
-      },
-      "project two" : {
-        "client" : "Company - IT dept.",
-        "desc" : "Tool supporting test process. Allows user to create tasks list per system version, application or business unit.",
-        "image" : "",
-        "name" : "Project two",
-        "order" : 2,
-        "other" : "internal application",
-        "period" : "2014",
-        "status" : "finished",
-        "subname" : "test task manager",
-        "tasks" : "analysis, layout, frontend code, tests",
-        "team" : "two people",
-        "technology" : ".Net, MS SQL, HTML, CSS, JavaScript",
-        "type" : "application"
-      }
-    },
-    "resume" : "https://github.com",
-    "skills" : "working in stressful environment; analytical skills; flexible; operating systems Windows; ASP.NET MVC, C#, MVVM, HTML5, CSS3, JavaScript (AngularJS), GruntJS, Gulp, Git; software prototyping; UI/UX;",
-    "trainings" : {
-      "training01" : "Training 01",
-      "training02" : "Training 02",
-      "training03" : "Training 03",
-      "training04" : "Training 04"
-    }
+              "born" : {
+                "icon" : "room",
+                "label" : "Born",
+                "name" : "Jan 1990",
+                "order" : 2
+              },
+              "email" : {
+                "email" : true,
+                "icon" : "email",
+                "label" : "Email",
+                "name" : "resume-project@patrykbe.pl",
+                "order" : 5
+              },
+              "family" : {
+                "icon" : "people",
+                "label" : "Family",
+                "name" : "wife, five kids",
+                "order" : 3
+              },
+              "name" : {
+                "icon" : "person",
+                "label" : "Name",
+                "name" : "John Doe",
+                "order" : 1
+              },
+              "phone" : {
+                "icon" : "call",
+                "label" : "Phone",
+                "name" : "+48 510217852",
+                "order" : 4
+              }
+            },
+            "education" : {
+              "u1" : {
+                "field" : "economy, speciality banking — MA",
+                "name" : "univeristy",
+                "order" : 1,
+                "when" : "2001 - 2002"
+              },
+              "u2" : {
+                "field" : "finance/banking, speciality banking — LIC",
+                "name" : "univeristy",
+                "order" : 2,
+                "when" : "1999 - 2001"
+              }
+            },
+            "experience" : {
+              "exp1" : {
+                "company" : "Company 1",
+                "order" : 2,
+                "position" : "IT Specialist",
+                "responsibilities" : {
+                  "res1" : "responsibility 1  ",
+                  "res2" : "responsibility 2 "
+                },
+                "when" : "Oct 1998 - Jan 1999 (4 mths)"
+              },
+              "exp2" : {
+                "company" : "Company 2",
+                "order" : 1,
+                "position" : "Analyst",
+                "responsibilities" : {
+                  "res1" : "responsibility 1  ",
+                  "res2" : "responsibility 2 "
+                },
+                "when" : "Nov 1999 - present (15 yrs)"
+              }
+            },
+            "faq" : {
+              "faq01" : {
+                "answer" : "anwser 1",
+                "label" : "Answer 1",
+                "order" : 1,
+                "question" : "Question 1?"
+              },
+              "faq02" : {
+                "answer" : "anwser 2",
+                "label" : "Answer 2",
+                "order" : 2,
+                "question" : "Question 2?"
+              },
+              "faq03" : {
+                "answer" : "anwser 3",
+                "label" : "Answer 3",
+                "order" : 3,
+                "question" : "Question 3?"
+              }
+            },
+            "interests" : "computer technology; electronic gadget news; winter sports; bikes; motor sport (wec, dtm, f1)",
+            "languages" : {
+              "de" : "german ",
+              "en" : "english"
+            },
+            "profile" : {
+              "desc" : "I am interested in work in the Information Systems area. I am interested in work in the Information Systems area. I am interested in work in the Information Systems area. <br><br>I am interested in work in the Information Systems area. I am interested in work in the Information Systems area. I am interested in work in the Information Systems area. "
+            },
+            "projects" : {
+              "project one" : {
+                "client" : "Company - IT dept.",
+                "desc" : "Tool supporting test process. Allows user to create tasks list per system version, application or business unit.",
+                "image" : "",
+                "name" : "Project one",
+                "order" : 1,
+                "other" : "internal application",
+                "period" : "2010/2011",
+                "status" : "finished",
+                "subname" : "test task manager",
+                "tasks" : "analysis, layout, frontend code, tests",
+                "team" : "two people",
+                "technology" : ".Net, MS SQL, HTML, CSS, JavaScript",
+                "type" : "application"
+              },
+              "project two" : {
+                "client" : "Company - IT dept.",
+                "desc" : "Tool supporting test process. Allows user to create tasks list per system version, application or business unit.",
+                "image" : "",
+                "name" : "Project two",
+                "order" : 2,
+                "other" : "internal application",
+                "period" : "2014",
+                "status" : "finished",
+                "subname" : "test task manager",
+                "tasks" : "analysis, layout, frontend code, tests",
+                "team" : "two people",
+                "technology" : ".Net, MS SQL, HTML, CSS, JavaScript",
+                "type" : "application"
+              }
+            },
+            "resume" : "https://github.com",
+            "skills" : "working in stressful environment; analytical skills; flexible; operating systems Windows; ASP.NET MVC, C#, MVVM, HTML5, CSS3, JavaScript (AngularJS), GruntJS, Gulp, Git; software prototyping; UI/UX;",
+            "trainings" : {
+              "training01" : "Training 01",
+              "training02" : "Training 02",
+              "training03" : "Training 03",
+              "training04" : "Training 04"
+            }
         }
     }
     
